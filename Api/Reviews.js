9c007fd1cec97ec981a9b4a73037af8ceb7b1531
DanielMalede <danielmalede@gmail.com>
2022-09-06 22:16:56 +0300

    .

--- Api/Reviews.js
+++ Api/Reviews.js
@@ -1,18 +1,17 @@
 async function getreviews() {
   try {
-    return await fetch('https://app.reviewapi.io/api/v1/reviews?apikey=b7200750-17f9-11ed-a6b8-7bb18d600c95&url=https%3A%2F%2Fwww.healthgrades.com%2Fdentist%2Fdr-alexander-milman-yvqbl&amount=12')
-      .then(response => response.json())
+    return await fetch(
+      "https://app.reviewapi.io/api/v1/reviews?apikey=b7200750-17f9-11ed-a6b8-7bb18d600c95&url=https%3A%2F%2Fwww.healthgrades.com%2Fdentist%2Fdr-alexander-milman-yvqbl&amount=12"
+    )
+      .then((response) => response.json())
       // .then(response => console.log(response))
-      .catch(err => console.error(err));
-  } catch (error) {
-  }
+      .catch((err) => console.error(err));
+  } catch (error) {}
 }
 function printReview() {
-  getreviews()
-    .then(item => {
-      item.reviews.forEach(element => {
-        console.log(element.text);
-
-      });
-    })
+  getreviews().then((item) => {
+    item.reviews.forEach((element) => {
+      console.log(element.text);
+    });
+  });
 }
