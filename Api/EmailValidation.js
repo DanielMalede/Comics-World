0af39d0115a6e312cec93aff7dc150a5a1059125
DanielMalede <danielmalede@gmail.com>
2022-09-06 22:16:28 +0300

    .

--- Api/EmailValidation.js
+++ Api/EmailValidation.js
@@ -1,39 +1,39 @@
 const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+  method: "GET",
+  redirect: "follow",
 };
-const EmailUser = document.getElementById("EmailUser")
+const EmailUser = document.getElementById("EmailUser");
 async function emailValidationApi() {
-    try {
-        return await fetch(`https://api.eva.pingutil.com/email?email=${EmailUser.value}`, requestOptions)
-            .then(response => response.json())
-    }
-    catch (err) {
-    }
+  try {
+    return await fetch(
+      `https://api.eva.pingutil.com/email?email=${EmailUser.value}`,
+      requestOptions
+    ).then((response) => response.json());
+  } catch (err) {}
 }
-async function reactionToEmailValidation() {
-    let response = await emailValidationApi();
-    console.log(response.data.webmail);
-    console.log(response.data.deliverable);
-    console.log(response.data);
-    if (response.data.webmail === true && response.data.deliverable === true) {
-        return true
-    } else {
-        return false
-    }
 
+async function reactionToEmailValidation() {
+  let response = await emailValidationApi();
+  console.log(response.data.webmail);
+  console.log(response.data.deliverable);
+  console.log(response.data);
+  if (response.data.webmail === true && response.data.deliverable === true) {
+    return true;
+  } else {
+    return false;
+  }
 }
 function actionOfPrintingUser() {
-    return (
-        window.location.href = "mailto:" + ('danielmalede@gmail.com')
-
-    )
+  return (window.location.href = "mailto:" + "danielmalede@gmail.com");
 }
 async function printingUserPage() {
-    let check = await reactionToEmailValidation()
-    if (check == true) {
-        return actionOfPrintingUser()
-    }
-    else { return alert("Invalid Email") }
+  let check = await reactionToEmailValidation();
+  if (check == true) {
+    return actionOfPrintingUser();
+  } else {
+    return alert("Invalid Email");
+  }
 }
-email_valid_btn.addEventListener("click", () => { printingUserPage() })
+email_valid_btn.addEventListener("click", () => {
+  printingUserPage();
+});
